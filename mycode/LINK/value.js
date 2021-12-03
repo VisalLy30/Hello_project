3dc6de57e184ed5583c4611b0d32ece826d29799
VisalLy30 <73353707+VisalLy30@users.noreply.github.com>
2021-12-03 16:53:03 +0700

    all code

---
+++ mycode/LINK/value.js
@@ -0,0 +1,5 @@
+document.write("5+6: " + (5 + 6));
+document.write("<br>15-6: " + (15 - 6));
+document.write("<br>15*6: " + (15 * 6));
+document.write("<br>15/6: " + (15 / 6));
+document.write("<br>15%6: " + (15 % 6));
